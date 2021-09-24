72c484af298ade082b5bec7ebaa82dbca3527249
ItsChurr0s <69118276+ItsChurr0s@users.noreply.github.com>
2021-09-23 23:13:02 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,48 @@
 // Show & hide genre selection
+let genreToggle = document.getElementById("form-by-genre");
+let actorToggle = document.getElementById("actor-search");
+let directorToggle = document.getElementById("director-search");
+
+
+const resettableElements = [
+    genreToggle,
+    actorToggle,
+    directorToggle,
+];
+
+function toggleElement(element) {
+    resettableElements.forEach((node) => {
+        node.style.display = "none";
+    });
+
+    element.style.display = "block";
+}
+
+// Show & hide genre selection
 function ShowHideGenre() {
     let genreToggle = document.getElementById("form-by-genre");
-    if (genreToggle.style.display === "block") {
-        genreToggle.style.display = "none";
-    } else {
-        genreToggle.style.display = "block";
-    }
+    toggleElement(genreToggle);
 }
 
+// Show & hide actor search
 function ShowHideActor() {
-    let genreToggle = document.getElementById("form-by-actor");
-    if (genreToggle.style.display === "block") {
-        genreToggle.style.display = "none";
-    } else {
-        genreToggle.style.display = "block";
-    }
+    let actorToggle = document.getElementById("actor-search");
+    toggleElement(actorToggle);
 }
 
+// Show & hide director search
 function ShowHideDirector() {
-    let genreToggle = document.getElementById("form-by-director");
-    if (genreToggle.style.display === "block") {
-        genreToggle.style.display = "none";
-    } else {
-        genreToggle.style.display = "block";
-    }
+    let directorToggle = document.getElementById("director-search");
+    toggleElement(directorToggle);
+}
+
+// Fetch movie
+async function FindMovie() {
+    const response = await fetch("https://api.themoviedb.org/3/movie/800000?api_key=e4fc096b1f7f3fbf48013349c7456fef&language=en-US");
+    const data = await response.json();
+    console.log(data);
+    title = data.title;
+    console.log(data.title);
+    title = document.getElementById("returnMovie").innerHTML = title;
+
 }
