a3859e6d4d0c429003b02744cebf336a3497e0a3
ItsChurr0s <69118276+ItsChurr0s@users.noreply.github.com>
2021-11-11 21:02:39 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -36,13 +36,41 @@ function ShowHideDirector() {
     toggleElement(directorToggle);
 }
 
+// Surprise Me Random Number Generator
+function SurpriseMe() {
+    let surpriseToggle = document.getElementById("surprise-me");
+    toggleElement(surpriseToggle);
+    randomID = Math.floor(Math.random() * 799999) + 1;
+    console.log(randomID);
+}
+
+// Backdrop
+function getBackdrop(backdrop) {
+    backdropURL = `https://img.tmdb.org/t/p/w500${backdrop}`;
+    backdropIMG = document.createElement('img');
+    backdropIMG.src = backdropURL;
+    backdropDiv = document.getElementById("backdropDiv");
+    backdropDiv.appendChild(backdropIMG);
+}
+
 // Fetch movie
 async function FindMovie() {
-    const response = await fetch("https://api.themoviedb.org/3/movie/800000?api_key=e4fc096b1f7f3fbf48013349c7456fef&language=en-US");
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${randomID}?api_key=e4fc096b1f7f3fbf48013349c7456fef&language=en-US`);
     const data = await response.json();
+    if (data.success == false) {
+        SurpriseMe();
+        FindMovie();
+    }
     console.log(data);
+    backdrop = data.backdrop_path;
     title = data.title;
+    overview = data.overview;
+    year = data.release_date;
     console.log(data.title);
+    console.log(backdrop);
+    backdrop = document.getElementById("backdropDiv").innerHTML = backdrop;
     title = document.getElementById("returnMovie").innerHTML = title;
+    overview = document.getElementById("returnOverview").innerHTML = overview;
+    year = document.getElementById("returnYear").innerHTML = year;
 
 }
